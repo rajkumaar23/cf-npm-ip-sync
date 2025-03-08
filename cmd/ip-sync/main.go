290927ba9380b80049ab92bfd3d85f0041fa1881
Rajkumar <rajkumaar2304@icloud.com>
2025-03-08 10:13:46 -0800

    update main

--- cmd/ip-sync/main.go
+++ cmd/ip-sync/main.go
@@ -7,11 +7,25 @@ import (
 )
 
 func main() {
+	c, err := internal.NewConfig()
+	if err != nil {
+		log.Fatalf("failed to create config: %v", err)
+	}
+
 	ctx := context.Background()
 	ips, err := internal.GetCloudflareIPs(ctx)
 	if err != nil {
 		log.Fatalf("failed to get cloudflare IPs: %v", err)
 	}
 
-	log.Printf("cloudflare IPs: %v", ips)
+	nc, err := internal.NewNPMClient(c.NPMHost, c.NPMEmail, c.NPMPassword)
+	if err != nil {
+		log.Fatalf("failed to create npm client: %v", err)
+	}
+	err = nc.UpdateAccessList(c.NPMAccessListID, ips)
+	if err != nil {
+		log.Fatalf("failed to update access list: %v", err)
+	}
+
+	log.Printf("successfully updated access list")
 }
